e9e0d2e89dde6b6e8f9bcab28178d1dc3eac0e26
Teseo15 <brayan.blas.egaovil30@gmail.com>
2021-11-16 09:37:46 -0500

    tarea 9 completada

--- api/components/auth/controller.js
+++ api/components/auth/controller.js
@@ -2,47 +2,72 @@ import { sign } from "../../../auth";
 import { store, findBy } from "../../../store/dummy";
 import { response } from "../../../network";
 import { hash, compare } from "../../../helper/encrypt";
-import userModel from "../user/model";
+import User from "../user/model";
+import fetch from "node-fetch";
+//const fetch = require('node-fetch');
 
 export const login = async (req, res) => {
-  // ?Destructuracion
-  const user = req.body;
+ // ?Destructuracion
+ const user = req.body;
+ console.log("SI FUNCIONA lOGIN")
+ //? Este payload se envia a sign para ser parte de la creacion del token
+ const payload = {
+   email: user.email,
+   name: user.name,
+   password: user.password,
+ };
 
-  //? Este payload se envia a sign para ser parte de la creacion del token
-  const payload = {
-    email: user.email,
-    password: user.password,
-  };
+ // const token =  sign(payload);
+  //console.log("SI FUNCIONA lOGIN despues de token", token)
+  
+/*
+ const token = app.post({url : "http://localhost:5000/auth/sign", form: payload})
+ 
+ console.log("DATOS TOKEN : ",token);
+*/
 
-  const token = sign(payload);
+const url = 'http://localhost:5000/auth/sign';
 
-  //? primer debo buscar a mi usario
-  const userData = await findBy(userModel, "email", user.email);
-  //? luego debo ver si existe
-  if (!userData) return;
-  //? luego comparo la contraseña
-  const validate = compare(user.password, userData.password);
+const respuesta = await fetch(url, {
+	method: 'post',
+	body: JSON.stringify(payload),
+	headers: {'Content-Type': 'application/json'}
+});
+const data = await respuesta.json();
+const token = data.data;
+console.log("TOKEN: ",token);
 
-  if (!validate) {
-    return response({
-      res,
-      ok: false,
-      status: 500,
-      data: {
-        message: "User invalid",
-      },
-    });
-  }
 
-  return response({
-    res,
-    data: {
-      user,
-      token,
-    },
-  });
+ //? primer debo buscar a mi usario
 
-  //? si es ok retorno al usuario con su token
+ //const userData = await findBy(User, "email", user.email);
+ const userData = await User.findOne({ email: req.body.email })
+ console.log("SI FUNCIONA userData", userData)
+ //? luego debo ver si existe
+ if (!userData) return;
+ console.log('existe')
+ //? luego comparo la contraseña
+ const validate = compare(user.password, userData.password);
+
+ if (!validate) {
+   return response({
+     res,
+     ok: false,
+     status: 500,
+     data: {
+       message: "User invalid",
+     },
+   });
+ }
+ console.log("SI FUNCIONA lOGIN YA ESTA EN LO ULTIMO")
+ return response({
+  res,
+  data: {
+    user,
+    token
+  },
+});
+ //? si es ok retorno al usuario con su token
 };
 
 export const signUp = async (req, res) => {
@@ -50,7 +75,7 @@ export const signUp = async (req, res) => {
 
   //* Creamos el data del usuario nuevo
   // borramos el id porque mongodb ya lo pone
-
+  console.log("SI FUNCIONA signup")
   const data = {
     name: user.name,
     last_name: user.last_name,
@@ -58,7 +83,7 @@ export const signUp = async (req, res) => {
     password: hash(user.password),
   };
 
-  const users = await store(userModel, data);
+  const users = await store(User, data);
 
   return response({ res, data: users, status: 201 });
 };
--- config/config.js
+++ config/config.js
@@ -3,13 +3,13 @@ export const base_url = "/api/v1";
 
 export const DOMAIN = process.env.DOMAIN || "http://localhost";
 
-export const port = process.env.PORT || 8080;
+export const port = process.env.PORT ||6000;
 
 export const AUTH_PORT = process.env.AUTH_PORT || 5000;
 
 export const secret = process.env.SECRET || "secret";
 
-const mongo_local = "mongodb://192.168.1.4:27017/local";
+const mongo_local = "mongodb+srv://teseo55:admin123@mini-merm.1oegf.mongodb.net/blogtech";
 // const mongo_remote =
 // "mongodb+srv://root:6ixiU2NTTdQUOoY4@cluster0.wns0c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
